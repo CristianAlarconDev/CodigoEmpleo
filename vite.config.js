e531568b160583e250b3857b6ced011cbf2d5de2
Ariel <ariel.oliva3748@gmail.com>
2025-10-10 14:22:27 -0300

    Separo los cursos y los trabajos en una pagina para cada seccion

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+
+// Configuración base
+export default defineConfig({
+  server: {
+    port: 5173, // Podés cambiar el puerto si querés
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        cursos: 'cursos.html', // 🔹 tu segunda página
+      },
+    },
+  },
+})

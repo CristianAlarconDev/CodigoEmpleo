8f5f2a0545bccee5f2dbfa0e8f9b1701ec2f8241
CristianAlarconDev <calarcon@fi.uba.ar>
2025-10-20 18:35:00 -0300

    se agregar filtro empleos para seniority, luego se va a implmentar para modalidad y otros

---
+++ src/filtro-empleos.js
@@ -0,0 +1,30 @@
+const contenedor = document.querySelector('#empleos-contenedor');
+const select = document.querySelector('#seniority-select');
+let cardsEmpleosBase = [];
+const obtenerCardsEmpleo = () => Array.from(contenedor.querySelectorAll('.card-empleo'));
+
+export function chequearCards() {
+  console.log(obtenerCardsEmpleo()[1]);
+}
+function cardDesdeSeniority(empleo, seniority){
+    return seniority === 'Todos' || empleo.dataset.seniority === seniority;
+}
+function aplicarFiltro(){
+    const filtroBuscado = select.value;
+    const cardsEmpleos = obtenerCardsEmpleo();
+    const cardsFiltradas =cardsEmpleosBase.filter(card => cardDesdeSeniority(card, filtroBuscado));
+    //console.log(cardsFiltradas[0]);
+    return cardsFiltradas;
+}
+function renderizarCardsFiltradas(){
+    const fragmento = document.createDocumentFragment();
+    const cardsFiltradas= aplicarFiltro();
+
+    cardsFiltradas.forEach(card => fragmento.appendChild(card));
+    contenedor.innerHTML = '';
+    contenedor.appendChild(fragmento);
+}
+export function inicializarFiltros(){
+    cardsEmpleosBase = obtenerCardsEmpleo();
+    select.addEventListener('change', renderizarCardsFiltradas);
+}

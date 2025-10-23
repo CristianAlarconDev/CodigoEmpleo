484303ab6bdd32addaafbaabc1e895b004c04aee
CristianAlarconDev <calarcon@fi.uba.ar>
2025-10-23 17:43:49 -0300

    se generalizo la funcion para filtrar, ahora combina filtro de seniority y modalidad; la funcion se agego con add event listener a cada elemento del DOM, tanto input de modalidad como el select de seniority

--- src/filtro-empleos.js
+++ src/filtro-empleos.js
@@ -10,30 +10,46 @@ export function chequearCards() {
   console.log(obtenerCardsEmpleo()[1]);
 }
 function cardDesdeSeniority(empleoCard, seniority){
-    //console.log(empleo.dataset.seniority);
-    //console.log(empleo);
+    
     return seniority === 'Todos' || empleoCard.dataset.seniority === seniority;
 }
-/*function cardDesdeModalidad(empleo, modalidadSeleccionadas){
-    return modalidadSeleccionadas.length === 0 || modalidadSeleccionadas.includes(empleo.dataset.modalidad);
-}*/
-/*
-function aplicarFiltroModalidad(){
-    const filtroBuscado = select.value;
-    const cardsEmpleos = obtenerCardsEmpleo();
-    const cardsFiltradas =cardsEmpleosBase.filter(card => cardDesdeModalidad(card, filtroBuscado));
-}*/
-function aplicarFiltroSeniority(filtroBuscado){
-    //const filtroBuscado = select.value;
-    //const cardsEmpleos = obtenerCardsEmpleo();
-    const cardsFiltradas =cardsEmpleosBase.filter(card => cardDesdeSeniority(card, filtroBuscado));
-    //console.log(cardsFiltradas[0]);
+function cardDesdeModalidad(empleo, modalidadSeleccionadas){
+    if(modalidadSeleccionadas.length===0){
+        console.log("no se marcaron modalidades")
+        return true;
+    }
+    return modalidadSeleccionadas.includes(empleo.dataset.modalidad);
+}
+function aplicarFiltrosCombinados(filtroSeniority, filtrosModalidad){
+
+    const cardsFiltradas = cardsEmpleosBase.filter(card => {
+       
+        const pasaSeniority = cardDesdeSeniority(card, filtroSeniority);
+        const pasaModalidad = cardDesdeModalidad(card, filtrosModalidad);
+        
+        return pasaSeniority && pasaModalidad; 
+    });
     return cardsFiltradas;
 }
+
+/*
+function aplicarFiltroSeniority(filtroBuscado){
+   
+    const cardsFiltradas =cardsEmpleosBase.filter(card => cardDesdeSeniority(card, filtroBuscado));
+   
+    return cardsFiltradas;
+}*/
+
 function renderizarCardsFiltradas(){
     const fragmento = document.createDocumentFragment();
+
     const filtroSeniorityBuscado = senioritySelect.value;
-    const cardsFiltradas= aplicarFiltroSeniority(filtroSeniorityBuscado);
+
+    const filtrosModalidadBuscados = Array.from(checkboxesModalidad)
+        .filter(checkbox => checkbox.checked)
+        .map(checkbox => checkbox.value);
+        
+    const cardsFiltradas= aplicarFiltrosCombinados(filtroSeniorityBuscado, filtrosModalidadBuscados);
 
     cardsFiltradas.forEach(card => fragmento.appendChild(card));
     contenedor.innerHTML = '';
@@ -41,5 +57,9 @@ function renderizarCardsFiltradas(){
 }
 export function inicializarFiltros(){
     cardsEmpleosBase = obtenerCardsEmpleo();
+    /*sumo add event para cada input, se lanzaria la funcion ante el cambio de cada input */
+    checkboxesModalidad.forEach(checkbox => {
+        checkbox.addEventListener('change', renderizarCardsFiltradas);
+    });
     senioritySelect.addEventListener('change', renderizarCardsFiltradas);
 }
